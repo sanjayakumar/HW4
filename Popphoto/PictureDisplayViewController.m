ed25f6a7717c3409de65509d879f4ea4288112dc
Sanjaya Kumar <sanjaya.kumar@gmail.com>
2012-07-28 13:10:01 -0700

    Fixed Requirement 8 (initial zoom to show as much of the photo as possible). The trick to get it working was hint 11 -- put geometry related stuff in viewWillAppear!

--- Popphoto/PictureDisplayViewController.m
+++ Popphoto/PictureDisplayViewController.m
@@ -21,46 +21,17 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+}
+
+- (void) viewWillAppear:(BOOL)animated
+{
     self.scrollView.delegate = self;
     self.imageView.image = self.actualImage;
     self.scrollView.contentSize = self.imageView.image.size;
     self.imageView.frame = CGRectMake(0, 0, self.imageView.image.size.width, self.imageView.image.size.height);
     self.navigationItem.title = self.imageTitle;
-
-    // testing
-    
-    NSLog(@"%@", self.imageTitle);
-    
-    NSLog(@"scrollview content size %f %f", self.scrollView.contentSize.width, self.scrollView.contentSize.height);
-    NSLog(@"scrollview bounds size %f %f", self.scrollView.bounds.size.width, self.scrollView.bounds.size.height);
-    
-    NSLog(@"imageview frame size %f %f", self.imageView.frame.size.width, self.imageView.frame.size.height);
-    NSLog(@"imageview bounds size %f %f", self.imageView.bounds.size.width, self.imageView.bounds.size.height);
-    
-    CGRect visibleRect = [self.imageView convertRect:self.imageView.bounds toView:self.scrollView];
-    NSLog(@"imageview frame in scrollview coordinates  %f %f", visibleRect.size.width, visibleRect.size.height);
-    
-    visibleRect = [self.imageView convertRect:self.imageView.frame toView:self.scrollView];
-    NSLog(@"imageview bounds in scrollview coordinates  %f %f", visibleRect.size.width, visibleRect.size.height);
-    
     [self.scrollView zoomToRect:self.imageView.frame animated:YES];
-    
-    NSLog(@" After zoom to rect");
-    NSLog(@"scrollview content size %f %f", self.scrollView.contentSize.width, self.scrollView.contentSize.height);
-    NSLog(@"scrollview bounds size %f %f", self.scrollView.bounds.size.width, self.scrollView.bounds.size.height);
-    
-    NSLog(@"imageview frame size %f %f", self.imageView.frame.size.width, self.imageView.frame.size.height);
-    NSLog(@"imageview bounds size %f %f", self.imageView.bounds.size.width, self.imageView.bounds.size.height);
-    
-    visibleRect = [self.imageView convertRect:self.imageView.bounds toView:self.scrollView];
-    NSLog(@"imageview frame in scrollview coordinates  %f %f", visibleRect.size.width, visibleRect.size.height);
-    
-    visibleRect = [self.imageView convertRect:self.imageView.frame toView:self.scrollView];
-    NSLog(@"imageview bounds in scrollview coordinates  %f %f", visibleRect.size.width, visibleRect.size.height);
-
-    NSLog(@" ");
 }
-
 - (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView
 {
     return self.imageView;
